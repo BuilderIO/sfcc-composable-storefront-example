ce4a07a9fdaaa224d95a83240c9596d99c991063
Aziz Abbas <aziz@builder.io>
2022-07-17 12:13:51 -0700

    better icon

--- app/components/blocks/einstein-products-grid/einstein-products-grid.builder.jsx
+++ app/components/blocks/einstein-products-grid/einstein-products-grid.builder.jsx
@@ -8,7 +8,7 @@ const EinsteinProductsGrid = loadable(() => import('./index'), {fallback})
 
 Builder.registerComponent(EinsteinProductsGrid, {
     name: 'EinsteinProductsGrid',
-    image: 'https://cdn.builder.io/api/v1/image/assets%2F1fa6810c36c54e87bfe1a6cc0f0be906%2F187c548d37a24f68adc65382ac7c553c',
+    image: 'https://cdn.builder.io/api/v1/image/assets%2Fd1ed12c3338144da8dd6b63b35d14c30%2F671167ab7faa41d59624c88acf109360',
     inputs: [
         {
             name: 'title',
